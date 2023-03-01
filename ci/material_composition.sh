9d11098643f442920e1219462e626244b19f4cfe
Anna Zaborowska <anna.zaborowska@cern.ch>
2023-03-01 16:03:24 +0100

    Adjust dimensions of material scan

--- ci/material_composition.sh
+++ ci/material_composition.sh
@@ -7,8 +7,8 @@ acts-install/bin/ActsAnalysisMaterialComposition \
       -i $input \
       -o $output \
       --sub-names beampipe pixel sstrips lstrips solenoid \
-      --sub-rmin 0:25:200:680:1100 \
-      --sub-rmax 25:200:680:1100:2000 \
-      --sub-zmin -3200:-3200:-3200:-3200:-3200 \
-      --sub-zmax 3200:3200:3200:3200:3200 \
+      --sub-rmin 0:25:202:720:1160 \
+      --sub-rmax 24.4:200:720:1140:1200 \
+      --sub-zmin -4000:-2400:-3150:-3150:-3000 \
+      --sub-zmax 4000:2400:3150:3150:3000 \
       -s
