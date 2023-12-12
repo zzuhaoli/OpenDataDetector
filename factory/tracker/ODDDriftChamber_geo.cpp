e2e47416b13d07ba4982d92d2a2a9b1231f21505
Tao Lin <lintao51@gmail.com>
2023-12-12 21:53:39 +0800

    Add A/U/V layers according to Xiaocong's talk.

--- factory/tracker/ODDDriftChamber_geo.cpp
+++ factory/tracker/ODDDriftChamber_geo.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <iostream>
 
 #include "DD4hep/DetFactoryHelper.h"
 #include "DD4hep/DetType.h"
@@ -9,29 +10,47 @@ using namespace dd4hep;
 
 
 static Ref_t create_element(Detector &oddd, xml_h xml,
-                            SensitiveDetector sens) {
+                                SensitiveDetector sens) {
 
-  xml_det_t x_det = xml;
-  std::string detName = x_det.nameStr();
+    xml_det_t x_det = xml;
+    std::string detName = x_det.nameStr();
 
-  // Make Volume
-  xml_comp_t x_det_tubs = x_det.child(_U(tubs));
+    // Make Volume
+    xml_comp_t x_det_tubs = x_det.child(_U(tubs));
 
-  // Make DetElement
-  DetElement driftChamberElement(detName, x_det.id());
+    // Make DetElement
+    DetElement driftChamberElement(detName, x_det.id());
 
-  std::string shapeName = x_det_tubs.nameStr();
-  Tube tubeShape(shapeName, x_det_tubs.rmin(), x_det_tubs.rmax(),
-                 x_det_tubs.dz());
-  Volume tubeVolume(detName, tubeShape,
-                    oddd.material(x_det_tubs.materialStr()));
-  tubeVolume.setVisAttributes(oddd, x_det.visStr());
+    // Envelope
+    std::string shapeName = x_det_tubs.nameStr();
+    Tube tubeShape(shapeName, x_det_tubs.rmin(), x_det_tubs.rmax(),
+                    x_det_tubs.dz());
+    Volume tubeVolume(detName, tubeShape,
+                        oddd.material(x_det_tubs.materialStr()));
+    tubeVolume.setVisAttributes(oddd, x_det.visStr());
 
-  // Place it in the mother
-  Volume motherVolume = oddd.pickMotherVolume(driftChamberElement);
-  PlacedVolume placedTube = motherVolume.placeVolume(tubeVolume);
-  placedTube.addPhysVolID(detName, driftChamberElement.id());
-  driftChamberElement.setPlacement(placedTube);
+    // Place it in the mother
+    Volume motherVolume = oddd.pickMotherVolume(driftChamberElement);
+    PlacedVolume placedTube = motherVolume.placeVolume(tubeVolume);
+    placedTube.addPhysVolID(detName, driftChamberElement.id());
+    driftChamberElement.setPlacement(placedTube);
+
+    // Construct super layers
+    std::cout << "START BUILDING SUPER LAYER" << std::endl;
+    size_t superlayerNum = 0;
+    if (x_det.hasChild(_U(layers))) {
+        for (xml_coll_t superlay(xml, _U(layers)); 
+            superlay; ++superlay, ++superlayerNum) {
+            xml_comp_t x_superlayer = superlay;
+            std::cout << "SUPER LAYER [" << superlayerNum << "]"
+                      << " type: " << x_superlayer.typeStr()
+                      << " n: " << x_superlayer.number()
+                      << " rmin: " << x_superlayer.rmin()
+                      << " rmax: " << x_superlayer.rmax()
+                      << std::endl;
+        }
+    }
+
 
   return driftChamberElement;
 
